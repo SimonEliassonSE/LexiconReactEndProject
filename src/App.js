65b6ab9b399923db7facfde947ba985d4ed55386
a18ismca <ismetcatovic@hotmail.se>
2022-10-31 16:02:20 +0100

    Installed react-bootstrap and added files.
    Added files and edited Homepage.jsx so that some "products are displayed.

--- src/App.js
+++ src/App.js
@@ -2,43 +2,25 @@
 import './App.css';
 import {Homepage} from './Components/Homepage';
 import {Products} from './Components/Products';
+import {NavigationBar} from './Components/NavigationBar';
 import {Cart} from './Components/Cart';
-import { Route,Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
+
+
   return (
     <div className="App">
-   <nav className="navbar navbar-expand-lg bg-light">
-   <img src={require('./Image/Logo.PNG')} alt="logo" width="60" height="50" />
-<ul className="navbar-nav">
-  <li className="nav-item- m-1">
-    <Link className="btn btn-light btn_outline-primary" to = "/">
-      <i className="fa fa-home"></i> Home
-    </Link>
-    <Link className="btn btn-light btn_outline-primary" to = "/Products">
-    <i class="fa fa-shopping-bag"></i> Products 
-    </Link>
-    <Link className="btn btn-light btn_outline-primary" to = "/Cart">
-    <i class="fa fa-shopping-cart"></i> Cart
-    </Link>
-
-  
-  </li>
-
-</ul>
-
-      </nav>
+     <NavigationBar></NavigationBar>
       <Routes>
+
         <Route index path='/'  element={ <Homepage/> } />
         <Route path='/Products' element={ <Products/> }/>
         <Route path='/Cart'  element={ <Cart/> } />
       
-       
-
-        
       </Routes>
-    
+      <div className='footer'>Ismet Catovic</div>
     </div>
   );
 }
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import myData from "./Data/ismet.json";
+import products from "./Data/ismet2.json";
 import joj from "../Image/Logo.PNG";
-
+import {Card, Col, Row} from "react-bootstrap"
 
 export class Homepage extends React.Component {
 
@@ -22,7 +23,26 @@ componentDidMount(){
    
 }
 
+
+
 render(){
+
+    const addProduct = () => {
+        alert("Product added.")
+    }
+
+    const sortProducts = () => {
+        if(products.id % 3 == 1){
+            return(
+                products.map((product) => (
+                    <h2>{product.id}</h2>
+                ))
+            )
+        } else(
+            <h4>Stimt gar ned</h4>
+        )
+    }
+
     const{
         
     } = this.state;
@@ -34,8 +54,9 @@ render(){
                     <h1 className="display-6">Web Shop</h1>
                 </div>
 
-                <div className="d-flex justify-content-start">
+                
 
+                <div className="d-flex justify-content-start">
                     <ul className="list-group list-unstyled list-group-flush">
                         {myData.map((category) => (
                             <li key={category.categoryId}>
@@ -43,31 +64,146 @@ render(){
                                     {category.name}
                                 </a>
                             </li>
+                            
                         )
 
                         )}
                     </ul>
 
+
+                    
+                    <div className="d-flex justify-content-center">
+                           <div className="container">
+                                {products.map((product) => (
+                                    <Row>
+                                        <Col key={product.productId}>
+                                        <Card border="dark">
+                                            <Card.Img variant="top" src={joj} style={{ width: 200, height: 150 }}></Card.Img>
+                                            <Card.Body>
+                                                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                                                    <span className="fs-2">{product.name}</span>
+                                                    <span className="ms-2 text-muted">{product.price}:-</span>
+                                                </Card.Title>
+                                                <Card.Text className="d-flex justify-content-start">
+                                                    {product.description}
+                                                </Card.Text>
+                                                <Card.Link href="http://localhost:3000/Products" className="btn btn-primary">Details</Card.Link>
+                                                <Card.Link onClick={addProduct} className="btn btn-outline-dark">Add to cart</Card.Link>
+                                            </Card.Body>
+                                        </Card>
+                                        </Col>
+                                    </Row>
+                                   
+                                )
+                                    )
+                                }
+                                <div className="row">
+                                    <div className="col">
+                                    
+                                    </div>
+                                    <div className="col">
+                                       
+                                    </div>
+                                    <div className="col">
+                                    
+                                    </div>
+                                </div>
+                               
+                            </div>
+                    </div>
+
+
                     <div className="d-flex justify-content-center">
                             <div className="container">
                                 <div className="row">
                                     <div className="col">
-                                    <img style={{ width: 200, height: 200 }} src={joj} className="rounded mx-auto d-block"></img>
-                                        899:-
+                                    <Card border="dark">
+                                            <Card.Img variant="top" src={joj} style={{ width: 200, height: 150 }}></Card.Img>
+                                            <Card.Body>
+                                                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                                                    <span className="fs-2">Kastrull</span>
+                                                    <span className="ms-2 text-muted">399:-</span>
+                                                </Card.Title>
+                                                <Card.Text className="d-flex justify-content-start">
+                                                    Kastrull för att koka
+                                                </Card.Text>
+                                                <Card.Link href="http://localhost:3000/Products" className="btn btn-primary">Details</Card.Link>
+                                                <Card.Link onClick={addProduct} className="btn btn-outline-dark">Add to cart</Card.Link>
+                                            </Card.Body>
+                                        </Card>
                                     </div>
                                     <div className="col">
-                                    <img style={{ width: 200, height: 200 }} src={joj} className="rounded mx-auto d-block"></img>
-
+                                        <Card border="dark">
+                                            <Card.Img variant="top" src={joj} style={{ width: 200, height: 150 }}></Card.Img>
+                                            <Card.Body>
+                                                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                                                    <span className="fs-2">Spis</span>
+                                                    <span className="ms-2 text-muted">1999:-</span>
+                                                </Card.Title>
+                                                <Card.Text className="d-flex justify-content-start">
+                                                    Spis för att koka eller steka
+                                                </Card.Text>
+                                                <Card.Link href="http://localhost:3000/Products" className="btn btn-primary">Details</Card.Link>
+                                                <Card.Link onClick={addProduct} className="btn btn-outline-dark">Add to cart</Card.Link>
+                                            </Card.Body>
+                                        </Card>
+                                    </div>
+                                    <div className="col">
+                                    <Card border="dark">
+                                            <Card.Img variant="top" src={joj} style={{ width: 200, height: 150 }}></Card.Img>
+                                            <Card.Body>
+                                                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                                                    <span className="fs-2">Ugn</span>
+                                                    <span className="ms-2 text-muted">3599:-</span>
+                                                </Card.Title>
+                                                <Card.Text className="d-flex justify-content-start">
+                                                    Ugn för att steka i ugn
+                                                </Card.Text>
+                                                <Card.Link href="http://localhost:3000/Products" className="btn btn-primary">Details</Card.Link>
+                                                <Card.Link onClick={addProduct} className="btn btn-outline-dark">Add to cart</Card.Link>
+                                            </Card.Body>
+                                        </Card>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col">
-                                    <img style={{ width: 200, height: 200 }} src={joj} className="rounded mx-auto d-block"></img>
+                                    <Card border="dark">
+                                            <Card.Img variant="top" src={joj} style={{ width: 200, height: 150 }}></Card.Img>
+                                            <Card.Body>
+                                                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                                                    <span className="fs-2">Sil</span>
+                                                    <span className="ms-2 text-muted">69:-</span>
+                                                </Card.Title>
+                                                <Card.Text className="d-flex justify-content-start">
+                                                    Sil för att droppa av vatten
+                                                </Card.Text>
+                                                <Card.Link href="http://localhost:3000/Products" className="btn btn-primary">Details</Card.Link>
+                                                <Card.Link onClick={addProduct} className="btn btn-outline-dark">Add to cart</Card.Link>
+                                            </Card.Body>
+                                        </Card>
 
                                     </div>
                                     <div className="col">
-                                    <img style={{ width: 200, height: 200 }} src={joj} className="rounded mx-auto d-block"></img>
+                                    <Card border="dark">
+                                            <Card.Img variant="top" src={joj} style={{ width: 200, height: 150 }}></Card.Img>
+                                            <Card.Body>
+                                                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                                                    <span className="fs-2">Kastrull</span>
+                                                    <span className="ms-2 text-muted">399:-</span>
+                                                </Card.Title>
+                                                <Card.Text className="d-flex justify-content-start">
+                                                    Kastrull för att koka
+                                                </Card.Text>
+                                                <Card.Link href="http://localhost:3000/Products" className="btn btn-primary">Details</Card.Link>
+                                                <Card.Link onClick={addProduct} className="btn btn-outline-dark">Add to cart</Card.Link>
+                                            </Card.Body>
+                                        </Card>
 
+                                    </div>
+                                    <div className="col">
+                                    <Card>
+                                    
+                                        </Card>
                                     </div>
                                 </div>
                             </div>
